bdc70b829f181b15d81b18976f348cc4c7ff0286
Olalekan24434 <120368445+Olalekan24434@users.noreply.github.com>
2026-03-09 17:53:48 +0100

    Update IRIS APP.py

--- IRIS APP.py
+++ IRIS APP.py
@@ -12,19 +12,19 @@ form = st.form("iris_form")
 
 form.subheader("Enter Flower Measurement")
 
-sepal_length = form.number_input(
+SepalLengthCm = form.number_input(
     "Sepal Length (cm)", min_value=4.0, max_value=8.0, value=5.1
 )
 
-sepal_width = form.number_input(
+SepalWidthCm = form.number_input(
     "Sepal Width (cm)", min_value=1.0, max_value=4.5, value=3.5
 )
 
-petal_length = form.number_input(
+PetalLengthCm = form.number_input(
     "Petal Length (cm)", min_value=1.0, max_value=7.0, value=5.0
 )
 
-petal_width = form.number_input(
+PetalWidthCm = form.number_input(
     "Petal Width (cm)", min_value=0.1, max_value=2.5, value=0.2
 )
 
@@ -33,13 +33,14 @@ submit_button = form.form_submit_button("Predict")
 if submit_button:
 
     input_data = pd.DataFrame({
-        "sepal length (cm)": [sepal_length],
-        "sepal width (cm)": [sepal_width],
-        "petal length (cm)": [petal_length],
-        "petal width (cm)": [petal_width]
+        "sepal length (cm)": [SepalLengthCm],
+        "sepal width (cm)": [SepalWidthCm],
+        "petal length (cm)": [PetalLengthCm],
+        "petal width (cm)": [PetalWidthCm]
     })
 
     prediction = model.predict(input_data)
 
     st.subheader("Prediction Result")
     st.success(f"Predicted Species: {prediction[0]}")
+
